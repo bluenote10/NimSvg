45036a90ff53a95b23d98b07af14565ed93f357d
Fabian Keller <github.100.fkeller@spamgourmet.com>
2017-09-18 23:30:33 +0200

    updated examples

--- examples/basic1.nim
+++ examples/basic1.nim
@@ -1,5 +1,5 @@
 import nimsvg
 
 buildSvgFile("examples/basic1.svg"):
-  svg(width=200, height=200):
+  svg(width=200, height=200, xmlns="http://www.w3.org/2000/svg", version="1.1"):
     circle(cx=100, cy=100, r=80, stroke="teal", `stroke-width`=4, fill="#DDD")
